6d44e2a6a49a52a4218a0c9b169bfbada92ac022
Vaibhavi23 <119424545+Vaishna20@users.noreply.github.com>
2022-12-05 21:13:31 +0530

    Add files via upload

---
+++ break.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,n;
+     printf("Enter number :");
+     scanf("%d",&n);
+ 
+    for(i=0;i<=n;i++)
+    {
+         printf("\n%d",i);
+        if(i==5)
+       
+        break;
+    }
+
+}
